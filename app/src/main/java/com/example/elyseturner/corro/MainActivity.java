7ab09ee529d017e58d74c199da001f57d1d63cf2
ELYSE <elyse.lopez@gmail.com>
2015-07-10 15:04:04 -0400

    styling colors file and new activity skeletons

--- app/src/main/java/com/example/elyseturner/corro/MainActivity.java
+++ app/src/main/java/com/example/elyseturner/corro/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -15,7 +16,6 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.plus.Plus;
 
@@ -35,7 +35,7 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
 
     private SignInButton btnSignIn;
     private Button pastSession, checkWeather, newRun;
-    private LinearLayout llProfile;
+    private LinearLayout llMainNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -48,7 +48,7 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
     private void settingLoginContentView() {
         btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
 
-        llProfile = (LinearLayout) findViewById(R.id.llMainNav);
+        llMainNav = (LinearLayout) findViewById(R.id.llMainNav);
         checkWeather = (Button) findViewById(R.id.btnWeather);
         newRun = (Button) findViewById(R.id.btnNewRun);
         pastSession = (Button) findViewById(R.id.btnPastRuns);
@@ -94,12 +94,31 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
                 signInWithGplus();
                 break;
             case R.id.btnWeather:
+                //call logic
+                //on connection success switch to new activity
+                //for now let's just let us switch to the new activity
+                Intent a = new Intent( MainActivity.this, HourlyWeather.class);
+                startActivity(a);
                 break;
             case R.id.btnNewRun:
+                //session logic
+                //on success switch to new activity
+                //for now let's just let us switch to the new activity
+                Intent b = new Intent( MainActivity.this, NewRun.class);
+                startActivity(b);
                 break;
             case R.id.btnPastRuns:
+                //session logic
+                //on success switch to new activity
+                //for now let's just let us switch to the new activity
+                Intent c = new Intent( MainActivity.this, RunningHistory.class);
+                startActivity(c);
                 break;
         }
+    }
+
+    private void getHourlyWeather() {
+
     }
 
     @Override
@@ -136,6 +155,7 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
         mSignInClicked = false;
         Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
 
+
         updateUI(true);
     }
 
@@ -148,10 +168,10 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
     private void updateUI(boolean isSignedIn) {
         if (isSignedIn) {
             btnSignIn.setVisibility(View.GONE);
-            llProfile.setVisibility(View.VISIBLE);
+            llMainNav.setVisibility(View.VISIBLE);
         } else {
             btnSignIn.setVisibility(View.VISIBLE);
-            llProfile.setVisibility(View.GONE);
+            llMainNav.setVisibility(View.GONE);
         }
     }
 
